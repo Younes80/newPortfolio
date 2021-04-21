584429be372bd0f36473cab1d161de1d4a1825fb
Younes80 <57212570+Younes80@users.noreply.github.com>
2021-04-21 05:40:24 +0200

    Dark Mode modif

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 const DarkMode = () => {
+	// const autoDarkMode = () => {
+	// 	let d = new Date();
+	// 	let h = d.getHours();
+
+	// 	if (h > 18 || h < 7) {
+	// 		var element = document.body;
+	// 		element.classList.toggle('dark-mode');
+	// 	}
+	// };
+
 	const handleClick = () => {
 		var element = document.body;
 		element.classList.toggle('dark-mode');
